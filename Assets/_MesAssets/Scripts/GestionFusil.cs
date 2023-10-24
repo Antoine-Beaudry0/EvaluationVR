57c06f7b8e1204cfd5656d1c318132d94a404252
Antoine Beaudry <antoine.beaudry.01@edu.cegeptr.qc.ca>
2023-10-24 12:09:18 -0400

    changements

--- Assets/_MesAssets/Scripts/GestionFusil.cs
+++ Assets/_MesAssets/Scripts/GestionFusil.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class GestionFusil : MonoBehaviour
 {
     [SerializeField] public Transform raycastSpawnPoint;
     [SerializeField] private AudioSource _son = default;
 
-
     RaycastHit frappe;
 
     bool boutton = false;
-    public void Update()
-    {
 
+    private void Start()
+    {
+        InvokeRepeating("CheckRaycast", 0f, 0.1f);
+    }
+
+    void CheckRaycast()
+    {
         if (Physics.Raycast(raycastSpawnPoint.position, raycastSpawnPoint.forward, out frappe, 10f) && (boutton == true))
         {
             Debug.Log("JE TIRE");
 
-            if (frappe.collider.CompareTag("Ballon"))
+            if (frappe.collider.tag == "Ballon")
             {
                 Destroy(frappe.collider.gameObject);
                 Debug.Log("+1");
@@ -34,8 +39,5 @@ public class GestionFusil : MonoBehaviour
         boutton = true;
         Ray rayonTir = new Ray(raycastSpawnPoint.position, raycastSpawnPoint.forward);
         Debug.DrawRay(rayonTir.origin, rayonTir.direction * 10f, Color.red, 1f);
-
-
     }
-
 }
